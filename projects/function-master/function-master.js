54005aa91f608f8d0c26c1684adb1956c48c5f0e
Alex H <aphebert9994@gmail.com>
2023-12-21 13:01:58 -0600

    completed function master

--- projects/function-master/function-master.js
+++ projects/function-master/function-master.js
@@ -3,103 +3,158 @@
 //////////////////////////////////////////////////////////////////////
 
 function objectValues(object) {
-
+    //initialize array
+    const output = [];
+    //itterate through object keys
+    for(let key in object){
+        //push value to array
+        output.push(object[key]);
+    }
+    //return array 
+    return output;
 } 
 
 //////////////////////////////////////////////////////////////////////
 // Function 2 - Keys to String ///////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//Should take an object and return all its keys in a string each separated with a space
 function keysToString(object) {
 
+    //return object key array joined with space
+    return Object.keys(object).join(" ")
+
+
+        //initialize array
+        const output = [];
+        //itterate through object keys
+        for(let key in object){
+            //push value to array
+            output.push(key);
+        }
+        //return array joined by spaces
+        return output.join(" ");
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 3 - Values to String /////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+// Should take an object and return all its string values in a string each separated with a space
 function valuesToString(object) {
-    
+    //initialize an output array
+    const output = [];
+    //itterate through object keys
+    for(let key in object){
+        //if corresponing value is a string
+        if(typeof object[key] === 'string'){
+            //push to output array
+            output.push(object[key]);
+        }
+    }
+    //return joined output
+    return output.join(" ");
+
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 4 - Array or Object //////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//Should take one argument and return 'array' if its an array and 'object' if its an object"
 function arrayOrObject(collection) {
-    
+    return Array.isArray(collection) ? 'array' : 'object';
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 5 - Capitalize Word //////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//Should take a string of one word, and return the word with its first letter capitalized
 function capitalizeWord(string) {
-    
+    return string[0].toUpperCase() + string.substring(1);
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 6 - Capitalize All Words /////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//Should take a string of words and return a string with all the words capitalized
 function capitalizeAllWords(string) {
-    
+    let arr = string.split(" "),
+        capitalArr = [];
+        arr.forEach((word) => capitalArr.push(capitalizeWord(word)));
+    return capitalArr.join(" ");
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 7 - Welcome Message //////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//Should take an object with a name property and return 'Welcome <Name>!'
 function welcomeMessage(object) {
-
+    return `Welcome ${capitalizeWord(object.name)}!`;
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 8 - Profile Info /////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//Should take an object with a name an a species and return '<Name> is a <Species>'
 function profileInfo(object) {
-
+    return `${capitalizeWord(object.name)} is a ${capitalizeWord(object.species)}`
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 9 - Maybe Noises /////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//Should take an object, if this object has a noises array 
+//return them as a string separated by a space, if there are no noises return 'there are no noises'
 function maybeNoises(object) {
-
+    if(Array.isArray(object.noises) && object.noises.length > 0){
+        return object.noises.join(" ")
+    }
+    return 'there are no noises'
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 10 - Has Words ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//Should take a string of words and a word and return true if <word> is in <string of words>, otherwise return false.
 function hasWord(string, word) {
-
+    return string.split(" ").includes(word)
 }
-
 //////////////////////////////////////////////////////////////////////
 // Function 11 - Add Friend //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//Should take a name and an object and add the name to the object's friends array 
+//return the object
 function addFriend (name, object) {
-
+    object.friends.push(name);
+    return object;
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 12 - Is Friend ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//Should take a name and an object and return true if <name> is a friend of <object> and false otherwise
 function isFriend(name, object) {
-
+    if(object.friends){
+        return object.friends.includes(name);
+    }
+    return false;
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 13 - Non-Friends /////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//Should take a name and a list of people, and return a list of all the names that <name> is not friends with
 function nonFriends(name, array) {
-
+    //initialize array
+    const output = [];
+    //itterate through array of people objects
+    for(let person of array){
+        //if name is friend of person 
+        if(!isFriend(name, person) && person.name !== name){
+            //push to output array
+            output.push(person.name)
+        }
+    }
+    //return output array
+    return output;
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -107,23 +162,33 @@ function nonFriends(name, array) {
 //////////////////////////////////////////////////////////////////////
 
 function updateObject(object, key, value) {
-
+    object[key] = value;
+    return object
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 15 - Remove Properties ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//Should take an object and an array of strings. Should remove any properties on <object> that are listed in <array>
 function removeProperties(object, array) {
-
+    //itterate through array
+    for(let element of array){
+        //if object has a key matching element
+        if(element in object){
+            //remove property from object
+            delete object[element];
+            }
+        }
+    //return object
+    return object
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 16 - Dedup ///////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//Should take an array and return an array with all the duplicates removed
 function dedup(array) {
-
+    return Array.from( new Set(array))
 }
 
 //////////////////////////////////////////////////////////////////////
